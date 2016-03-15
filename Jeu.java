ab3d186f9dfde1dbc7614585754630e3916e3191
Loïc <lolo.49400@hotmail.fr>
2016-03-15 17:30:39 +0100

    nouvelle version

--- Jeu.java
+++ Jeu.java
@@ -1,3 +1,37 @@
-class Jeu {
+public class Jeu {
+  public Jeu(){
+    this.initialiserPlateau();
+  }
 
+  public void initialiserPlateau(){
+
+  }
+
+  public void jouer(Joueur j){
+
+  }
+
+  private void initialiserJoueur() {
+
+  }
+
+  public boolean deplacementPossible(int coordDepartX, int coordDepartY, int coordArriveX, int coordArriveY, Joueur j){
+
+  }
+
+  public boolean deplacer(int coordDepartX, int coordDepartY, int coordArriveX, int coordArriveY, Joueur j){
+
+  }
+
+  public Joueur joueurVainqueur(){
+
+  }
+
+  public boolean arretPartie(){
+
+  }
+
+  public String toString(){
+    
+  }
 }
